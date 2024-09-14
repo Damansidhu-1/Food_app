1954d7ce04e3104020c2ba2e9d744e2575cb5bbc
Damandeep Singh <deepdaman46@gmail.com>
2024-09-14 19:37:02 +0530

    routers and controllers related to user is created

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB";
+import userRoute from "./routes/user.route"
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 
 dotenv.config();
 
-connectDB();
 const PORT = process.env.PORT || 3000;
+// middlewares
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+    origin: "https://localhost:5173",
+    credentials: true
+}
+app.use(cors(corsOptions));
+
+app.use("/api/v1/user", userRoute);
+
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server listen at port ${PORT}`);
 });
